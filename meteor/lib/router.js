6fcbc16eac5f7cc37c384a187348ed315e0e7f71
dschadlich <dschadlich@gmail.com>
2016-04-06 22:25:17 -0400

    simple handling of options query
    Former-commit-id: f03c7771e8ab3afe32149959d970d384effa8141

--- meteor/lib/router.js
+++ meteor/lib/router.js
@@ -1,3 +1,12 @@
-Router.route( '/api/v1/add', function() {
+Router.route( '/api/v1', function() {
   // This is where we handle the request.
+  this.response.setHeader( 'Access-Control-Allow-Origin', '*' );
+
+  if ( this.request.method === "OPTIONS" ) {
+    this.response.setHeader( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept' );
+    this.response.setHeader( 'Access-Control-Allow-Methods', 'PUT' );
+    this.response.end( 'Set OPTIONS.' );
+  } else {
+    API.handleRequest( this, 'pizza', this.request.method );
+  }
 }, { where: 'server' } );
