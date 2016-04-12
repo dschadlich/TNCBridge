f8823555f02e2bbde4363981035f4e395d0bbd62
dschadlich <dschadlich@gmail.com>
2016-04-12 00:13:10 -0400

    playing with cursor

--- meteor/client/chart/chart.js
+++ meteor/client/chart/chart.js
@@ -4,14 +4,33 @@ Template.chart.helpers ({
   }
 
 });
+Template.formatd = function (){
+   console.log ("formatd");
+  let d =  Markers.find ({});
+
+  let count = 1;
+  let results = [];
+  d.forEach(function (marker) {
+    console.log ("foreach run");
+  console.log(count + ":" + marker.altitude);
+  results.push ([count, marker.altitude]);
+//need to make a subscription and then use the on ready
+  count += 1;
+  });
+
+  //console.log (d);
+
+  //console.log (results);
+  //return results;
+};
 
 Template.chart.rendered = function () {
   console.log ("chart rendered");
-  console.log ();
-  //formatd ();
-  let d = Markers.find ({}).fetch ();
-  $("#graph").plot(d,  {
-    xaxis: { mode: "time" }
-  });
+Template.formatd ();
+
+//  $.plot("#graph", [ Template.formatd () ]);
+
+
+  //.setData(d2).setupGrid().draw ();
 
 }
--- meteor/lib/collections.js
+++ meteor/lib/collections.js
@@ -1,1 +1,13 @@
 Markers = new Mongo.Collection('markers');
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish('markers', function chartData() {
+    return Markers.find({});
+  });
+
+  Meteor.publish('markers', function mapData() {
+    return Markers.find({});
+  });
+
+
+}
