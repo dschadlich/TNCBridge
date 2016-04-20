59eedca20c7ffa5db3f428226392ce20ab0796f2
dschadlich <dschadlich@gmail.com>
2016-04-19 22:12:59 -0400

    Have data showing up on the page !

--- meteor/client/chart/chart.js
+++ meteor/client/chart/chart.js
@@ -1,52 +1,70 @@
-Template.chart.helpers ({
-  formatd: function (input){
-    console.log ("format d called");
-  }
+// Template.chart.helpers ({
+//   formatd: function (input){
+//     console.log ("format d called");
+//   }
+//
+// });
+// Template.formatd = function (){
+//    console.log ("formatd");
+//   let d =  Markers.find ({}).fetch ();
+//   console.log (d);
+//   //need to make a subscription and then use the on ready
+//
+//   let count = 1;
+//   let results = [];
+//   Markers.find ({}).forEach(function (marker) {
+//     console.log(count + ":" + marker.altitude);
+//     results.push ([count, marker.altitude]);
+//     count += 1;
+//   });
+//
+//   //console.log (d);
+//
+//   //console.log (results);
+//   //return results;
+// };
+// Template.chart.onRendered(function() {
+//     console.log ("chart rendered");
+//     Meteor.setTimeout(findMarkers, 10);
+//   Template.chart.onCreated (function(){
+//     this.subscribe('markers');
+//   });
+//
+//
+//
+//
+// });
+// function findMarkers (){
+//   // console.log ("findMarkers run");
+//   // let d =  Markers.find ({}).fetch ();
+//   // console.log (d);
+//
+//
+//
+//   let count = 1;
+//   let results = [];
+//   Markers.find ({}).forEach(function (marker) {
+//     console.log(count + ":" + marker.altitude);
+//     results.push ([count, marker.altitude]);
+//     count += 1;
+//   });
+//
+// }
 
+
+
+
+
+
+Template.body.onRendered(function bodyOnCreated() {
+  Meteor.subscribe('packets');
+  Packets.find ().observe ({
+    added:function (NewDoc){
+      console.log ("new Data");
+      console.log (NewDoc);
+      $("#packetLog").append (NewDoc.Latitude.DD + "  " + NewDoc.Longitude.DD + "<br />\n");
+    }
+  });
+
+  //console.log (Meteor.packets;
 });
-Template.formatd = function (){
-   console.log ("formatd");
-  let d =  Markers.find ({}).fetch ();
-  console.log (d);
-  //need to make a subscription and then use the on ready
-
-  let count = 1;
-  let results = [];
-  Markers.find ({}).forEach(function (marker) {
-    console.log(count + ":" + marker.altitude);
-    results.push ([count, marker.altitude]);
-    count += 1;
-  });
-
-  //console.log (d);
-
-  //console.log (results);
-  //return results;
-};
-Template.chart.onRendered(function() {
-    console.log ("chart rendered");
-    Meteor.setTimeout(findMarkers, 10);
-  Template.chart.onCreated (function(){
-    this.subscribe('markers');
-  });
-
-
-
-
-});
-function findMarkers (){
-  // console.log ("findMarkers run");
-  // let d =  Markers.find ({}).fetch ();
-  // console.log (d);
-
-
-
-  let count = 1;
-  let results = [];
-  Markers.find ({}).forEach(function (marker) {
-    console.log(count + ":" + marker.altitude);
-    results.push ([count, marker.altitude]);
-    count += 1;
-  });
-
-}
--- meteor/lib/collections.js
+++ meteor/lib/collections.js
@@ -1,20 +1,22 @@
 Markers = new Mongo.Collection('markers');
 
 
-Packets = new Mongo.Collection( 'packets' );
-
-
-
-
-
-
-
-
-
+ Packets = new Mongo.Collection( 'packets' );
 
 
 
 if (Meteor.isServer) {
+
+
+
+
+
+  Meteor.publish('packets', function getPackets() {
+    return Packets.find({});
+  });
+
+
+
   // This code only runs on the server
   Meteor.publish('markers', function chartData() {
     return Markers.find({});
