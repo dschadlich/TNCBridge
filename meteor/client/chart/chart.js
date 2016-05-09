68024342260326b510cfe8a9fe4c1f58b7f05dbb
dschadlich <dschadlich@gmail.com>
2016-05-09 17:48:52 -0400

    added altitude info in place of the graph

--- meteor/client/chart/chart.js
+++ meteor/client/chart/chart.js
@@ -62,7 +62,7 @@ Template.chart.onRendered(function () {
     added:function (NewDoc){
     //  console.log ("new location");
     //  console.log (NewDoc);
-      $("#packetLog").append (NewDoc.Latitude.DD + "  " + NewDoc.Longitude.DD + "<br />\n");
+      $("#altitudeLog").html ("<h2> Current Altitude" + NewDoc.Latitude.altitude + "</h2>\n");
     }
   });
 
